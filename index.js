9e3317d13505f486013ea9adafd0e49baa6fb613
lionard <leonard.muhati@student.moringaschool.com>
2024-09-05 22:18:51 +0300

    compelete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+/** A book object representation.
+ * @typedef {Object} Book
+ * @property {string} title - The title of the book.
+ * @property {string} author - The author of the book.
+ * @property {string} genre - The genre of the book.
+ */
+function filterByGenre(genre,books){
+    return (
+        books.filter(book => book.genre === genre)
+
+     )
+}
